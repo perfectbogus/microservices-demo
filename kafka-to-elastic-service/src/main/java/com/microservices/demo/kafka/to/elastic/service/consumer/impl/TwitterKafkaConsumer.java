f415e91d883459115f0f52bc97dbea314dae325f
perfectbogus <war_army_10@hotmail.com>
2022-09-04 22:09:58 -0500

    S4:C36 - Running Elastic Search with Docker

--- kafka-to-elastic-service/src/main/java/com/microservices/demo/kafka/to/elastic/service/consumer/impl/TwitterKafkaConsumer.java
+++ kafka-to-elastic-service/src/main/java/com/microservices/demo/kafka/to/elastic/service/consumer/impl/TwitterKafkaConsumer.java
@@ -6,6 +6,8 @@ import com.microservices.demo.kafka.avro.model.TwitterAvroModel;
 import com.microservices.demo.kafka.to.elastic.service.consumer.KafkaConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.KafkaListenerEndpointRegistry;
 import org.springframework.kafka.support.KafkaHeaders;
@@ -27,6 +29,13 @@ public class TwitterKafkaConsumer implements KafkaConsumer<Long, TwitterAvroMode
     this.kafkaListenerEndpointRegistry = kafkaListenerEndpointRegistry;
     this.kafkaAdminClient = kafkaAdminClient;
     this.kafkaConfigData = kafkaConfigData;
+  }
+
+  @EventListener
+  public void onAppStarted(ApplicationStartedEvent event) {
+     kafkaAdminClient.checkTopicsCreated();
+     LOG.info("Topics with name {} is ready for operations", kafkaConfigData.getTopicNamesToCreate().toArray());
+     kafkaListenerEndpointRegistry.getListenerContainer("twitterTopicListener").start();
   }
 
   @KafkaListener(id = "twitterTopicListener", topics = "${kafka-config.topic-name}")
