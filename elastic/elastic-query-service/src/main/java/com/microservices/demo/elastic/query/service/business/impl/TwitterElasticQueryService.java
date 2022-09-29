c4b8715008cbf2bfa4e1f873bc9184faecddbc70
perfectbogus <war_army_10@hotmail.com>
2022-09-29 15:42:26 -0500

    S6:C51 Adding Hateos Capability to Rest API Spring Hateoas

--- elastic/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/business/impl/TwitterElasticQueryService.java
+++ elastic/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/business/impl/TwitterElasticQueryService.java
@@ -4,7 +4,7 @@ import com.microservices.demo.elastic.model.index.impl.TwitterIndexModel;
 import com.microservices.demo.elastic.query.client.service.ElasticQueryClient;
 import com.microservices.demo.elastic.query.service.business.ElasticQueryService;
 import com.microservices.demo.elastic.query.service.model.ElasticQueryServiceResponseModel;
-import com.microservices.demo.elastic.query.service.transformer.ElasticToResponseModelTransformer;
+import com.microservices.demo.elastic.query.service.model.assambler.ElasticQueryServiceResponseModelAssembler;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -16,29 +16,29 @@ public class TwitterElasticQueryService implements ElasticQueryService {
 
   private static final Logger LOG = LoggerFactory.getLogger(TwitterElasticQueryService.class);
 
-  private final ElasticToResponseModelTransformer elasticToResponseModelTransformer;
+  private final ElasticQueryServiceResponseModelAssembler elasticQueryServiceResponseModelAssembler;
   private final ElasticQueryClient<TwitterIndexModel> elasticQueryClient;
 
-  public TwitterElasticQueryService(ElasticToResponseModelTransformer elasticToResponseModelTransformer, ElasticQueryClient<TwitterIndexModel> elasticQueryClient) {
-    this.elasticToResponseModelTransformer = elasticToResponseModelTransformer;
+  public TwitterElasticQueryService(ElasticQueryServiceResponseModelAssembler elasticQueryServiceResponseModelAssembler, ElasticQueryClient<TwitterIndexModel> elasticQueryClient) {
+    this.elasticQueryServiceResponseModelAssembler = elasticQueryServiceResponseModelAssembler;
     this.elasticQueryClient = elasticQueryClient;
   }
 
   @Override
   public ElasticQueryServiceResponseModel getDocumentById(String id) {
     LOG.info("Querying elasticsearch by id {}", id);
-    return elasticToResponseModelTransformer.getResponseModel(elasticQueryClient.getIndexModelById(id));
+    return elasticQueryServiceResponseModelAssembler.toModel(elasticQueryClient.getIndexModelById(id));
   }
 
   @Override
   public List<ElasticQueryServiceResponseModel> getDocumentByText(String text) {
     LOG.info("Querying elasticsearch by text {}", text);
-    return elasticToResponseModelTransformer.getResponseModels(elasticQueryClient.getIndexModelByText(text));
+    return elasticQueryServiceResponseModelAssembler.toModels(elasticQueryClient.getIndexModelByText(text));
   }
 
   @Override
   public List<ElasticQueryServiceResponseModel> getAllDocuments() {
     LOG.info("Querying all documents in elasticsearch");
-    return elasticToResponseModelTransformer.getResponseModels(elasticQueryClient.getAllIndexModels());
+    return elasticQueryServiceResponseModelAssembler.toModels(elasticQueryClient.getAllIndexModels());
   }
 }
--- elastic/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/assambler/ElasticQueryServiceResponseModelAssembler.java
+++ elastic/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/model/assambler/ElasticQueryServiceResponseModelAssembler.java
@@ -7,6 +7,12 @@ import com.microservices.demo.elastic.query.service.transformer.ElasticToRespons
 import org.springframework.hateoas.server.mvc.RepresentationModelAssemblerSupport;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 @Component
 public class ElasticQueryServiceResponseModelAssembler extends RepresentationModelAssemblerSupport<TwitterIndexModel, ElasticQueryServiceResponseModel> {
 
@@ -19,7 +25,22 @@ public class ElasticQueryServiceResponseModelAssembler extends RepresentationMod
 
 
   @Override
-  public ElasticQueryServiceResponseModel toModel(TwitterIndexModel entity) {
-    return null;
+  public ElasticQueryServiceResponseModel toModel(TwitterIndexModel twitterIndexModel) {
+    ElasticQueryServiceResponseModel responseModel =
+            elasticToResponseModelTransformer.getResponseModel(twitterIndexModel);
+    responseModel.add(
+            linkTo(methodOn(ElasticDocumentController.class)
+                    .getDocumentById((twitterIndexModel.getId())))
+                    .withSelfRel()
+    );
+    responseModel.add(
+            linkTo(ElasticDocumentController.class)
+                    .withRel("documents")
+    );
+    return responseModel;
+  }
+
+  public List<ElasticQueryServiceResponseModel> toModels(List<TwitterIndexModel> twitterIndexModels) {
+    return twitterIndexModels.stream().map(this::toModel).collect(Collectors.toList());
   }
 }
