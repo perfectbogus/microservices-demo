c9239a169a11cc5a34402cccc71a63f9007baee3
perfectbogus <war_army_10@hotmail.com>
2023-01-17 15:55:44 -0600

    S7:C59 Creating Rest API Controller

---
+++ elastic/elastic-query-web-client/src/main/java/com/microservices/demo/elastic/query/web/client/exception/ElasticQueryWebClientException.java
@@ -0,0 +1,15 @@
+package com.microservices.demo.elastic.query.web.client.exception;
+
+public class ElasticQueryWebClientException extends RuntimeException {
+  public ElasticQueryWebClientException() {
+    super();
+  }
+
+  public ElasticQueryWebClientException(String message) {
+    super(message);
+  }
+
+  public ElasticQueryWebClientException(String message, Throwable throwable) {
+    super(message, throwable);
+  }
+}
