b64b7a23b51ce0f027e46621e271c023d20b712a
eduardo escobar <war_army_10@hotmail.com>
2023-03-19 19:50:15 -0600

    S9:C70 - Configuring Query Service with Spring Security OAuth 2.0 OpenId Connect and Jwt

--- elastic/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
+++ elastic/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/api/ElasticDocumentController.java
@@ -13,6 +13,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,6 +26,7 @@ import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
 import java.util.List;
 
+@PreAuthorize("isAuthenticated()")
 @RestController
 @RequestMapping(value = "/documents" , produces = "application/vnd.api.v1+json")
 public class ElasticDocumentController {
@@ -85,6 +87,7 @@ public class ElasticDocumentController {
     return ResponseEntity.ok(responseModelV2);
   }
 
+  @PreAuthorize("hasRole('APP_USER_ROLE') || hasAuthority('SCOPE_APP_USER_ROLE')")
   @Operation(summary = "Get all elastic By text")
   @ApiResponses(value = {
           @ApiResponse(responseCode = "200", description = "success", content = {
--- elastic/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
+++ elastic/elastic-query-service/src/main/java/com/microservices/demo/elastic/query/service/config/WebSecurityConfig.java
@@ -1,27 +1,40 @@
 package com.microservices.demo.elastic.query.service.config;
 
 import com.microservices.demo.config.UserConfigData;
+import com.microservices.demo.elastic.query.service.security.TwitterQueryUserDetailsService;
+import com.microservices.demo.elastic.query.service.security.TwitterQueryUserJwtConverter;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
 
 @Configuration
-@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  private final UserConfigData userconfigData;
+  private final TwitterQueryUserDetailsService twitterQueryUserDetailsService;
+  private final OAuth2ResourceServerProperties oAuth2ResourceServerProperties;
   @Value("${security.paths-to-ignore}")
   private String[] pathsToIgnore;
 
-  public WebSecurityConfig(UserConfigData userconfigData) {
-    this.userconfigData = userconfigData;
+  public WebSecurityConfig(TwitterQueryUserDetailsService twitterQueryUserDetailsService,
+                           OAuth2ResourceServerProperties oAuth2ResourceServerProperties) {
+    this.twitterQueryUserDetailsService = twitterQueryUserDetailsService;
+    this.oAuth2ResourceServerProperties = oAuth2ResourceServerProperties;
   }
 
   @Override
@@ -33,20 +46,40 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   public void configure(HttpSecurity http) throws Exception {
-    http.httpBasic()
+    http
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and()
-            .authorizeRequests().antMatchers("/**").hasRole("USER")
+            .csrf()
+            .disable()
+            .authorizeRequests()
+            .anyRequest()
+            .fullyAuthenticated()
             .and()
-            .csrf().disable();
+            .oauth2ResourceServer()
+            .jwt()
+            .jwtAuthenticationConverter(twitterQueryUserJwtConverter());
   }
 
-  @Override
-  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth.inMemoryAuthentication()
-            .withUser(userconfigData.getUsername())
-            .password(passwordEncoder().encode(userconfigData.getPassword()))
-            .roles(userconfigData.getRoles());
+  @Bean
+  JwtDecoder jwtDecoder(@Qualifier("elastic-query-service-audience-validator")
+                        OAuth2TokenValidator<Jwt> audienceValidator) {
+    NimbusJwtDecoder jwtDecoder = (NimbusJwtDecoder) JwtDecoders.fromOidcIssuerLocation(
+            oAuth2ResourceServerProperties.getJwt().getIssuerUri()
+    );
+    OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(
+            oAuth2ResourceServerProperties.getJwt().getIssuerUri()
+    );
+    OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, audienceValidator);
+    jwtDecoder.setJwtValidator(withAudience);
+    return jwtDecoder;
   }
+
+  @Bean
+  Converter<Jwt, ? extends AbstractAuthenticationToken> twitterQueryUserJwtConverter() {
+    return new TwitterQueryUserJwtConverter(twitterQueryUserDetailsService);
+  }
+
 
   @Bean
   protected PasswordEncoder passwordEncoder() {
